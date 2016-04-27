8fa5c43e29466cf4593360641a63a0878e94f479
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2016-04-26 22:27:42 -0300

    Configurações

--- bs/wwwroot/_references.js
+++ bs/wwwroot/_references.js
@@ -4,3 +4,7 @@
 /// <reference path="../scripts/fallback/styles.js" />
 /// <reference path="../scripts/site.js" />
 /// <reference path="../test/site-test.js" />
+/// <reference path="js/bootstrap.js" />
+/// <reference path="js/jquery.js" />
+/// <reference path="js/jquery-validate-unobtrusive.js" />
+/// <reference path="js/site.js" />
