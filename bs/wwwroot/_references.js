e687bb70f75f7b2abb5dd793cf8a28a47a6fbddd
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2016-04-26 22:55:19 -0300

    Novos scripts adicionados na referência

--- bs/wwwroot/_references.js
+++ bs/wwwroot/_references.js
@@ -4,7 +4,3 @@
 /// <reference path="../scripts/fallback/styles.js" />
 /// <reference path="../scripts/site.js" />
 /// <reference path="../test/site-test.js" />
-/// <reference path="js/bootstrap.js" />
-/// <reference path="js/jquery.js" />
-/// <reference path="js/jquery-validate-unobtrusive.js" />
-/// <reference path="js/site.js" />
